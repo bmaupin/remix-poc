afabec95cfcd7be5a8467855a5668c4ed2bbe892
bmaupin <bmaupin@users.noreply.github.com>
2022-01-28 12:49:23 -0500

    feat: Read posts from the filesystem

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import { getPost } from '~/post';
+import invariant from 'tiny-invariant';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, 'expected params.slug');
+  return getPost(params.slug);
+};
+
+export default function PostSlug() {
+  const post = useLoaderData();
+
+  console.log('post=', post);
+
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  );
+}
