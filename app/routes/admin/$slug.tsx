865868f285d06adea4f15d72c50caf8a324ae8f4
bmaupin <bmaupin@users.noreply.github.com>
2022-02-03 13:07:08 -0500

    feat: Reuse action from new inside edit route

--- app/routes/admin/$slug.tsx
+++ app/routes/admin/$slug.tsx
@@ -3,49 +3,19 @@ import {
   useLoaderData,
   useTransition,
   Form,
-  redirect,
   LoaderFunction,
 } from 'remix';
-import type { ActionFunction } from 'remix';
 import invariant from 'tiny-invariant';
-import { createPost, getPost } from '~/post';
 
-type PostError = {
-  title?: boolean;
-  slug?: boolean;
-  markdown?: boolean;
-};
+import { action as newPostAction } from './new';
+import { getPost } from '~/post';
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, 'expected params.slug');
   return getPost(params.slug);
 };
 
-export const action: ActionFunction = async ({ request }) => {
-  await new Promise((res) => setTimeout(res, 1000));
-
-  const formData = await request.formData();
-
-  const title = formData.get('title');
-  const slug = formData.get('slug');
-  const markdown = formData.get('markdown');
-
-  const errors: PostError = {};
-  if (!title) errors.title = true;
-  if (!slug) errors.slug = true;
-  if (!markdown) errors.markdown = true;
-
-  if (Object.keys(errors).length) {
-    return errors;
-  }
-
-  invariant(typeof title === 'string');
-  invariant(typeof slug === 'string');
-  invariant(typeof markdown === 'string');
-  await createPost({ title, slug, markdown });
-
-  return redirect('/admin');
-};
+export const action = newPostAction;
 
 export default function EditPost() {
   const errors = useActionData();
